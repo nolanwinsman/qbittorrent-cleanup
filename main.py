2cb4746f584b6e138d8b580203f674714f7cb72e
nolanwinsman <nolanwinsman@gmail.com>
2025-05-18 16:30:43 +0000

    basic script completed

--- main.py
+++ main.py
@@ -8,14 +8,20 @@
 
 
 import qbittorrentapi # pip install qbittorrent-api
+from datetime import datetime
 
 # documentation to API
 # https://qbittorrent-api.readthedocs.io/en/latest/introduction.html
 
+CATEGORIES = set()
+DELETE_FILES = True
+THRESHOLD = 90 # will delete any torrent older than this many days
 
 # dictionary to map torrent tags to folders
 # the key is the torrent tag and the value is the folder it should be put in
 def main():
+
+    today = datetime.today()
 
     # instantiate a Client using the appropriate WebUI configuration
     # change information to fit your login
@@ -33,9 +39,26 @@ def main():
     except qbittorrentapi.LoginFailed as e:
         print(e)
 
+    CATEGORIES.add('tv-sonarr')
+    CATEGORIES.add('radarr')
+
+    count = 0
     # retrieve all torrents and set their download folder to the corresponding tag
     for torrent in qbt_client.torrents_info():
-        print(f'{torrent.name} ({torrent.state}) ({torrent.tags})')
+        dt = datetime.fromtimestamp(torrent.added_on)
+        added_on = dt.strftime("%m/%d/%Y")
+        c = torrent.category
+
+        if (today - dt).days > THRESHOLD and c in CATEGORIES:
+            count +=  1
+            print("Added Greater than 90 days ago and category matches")
+            print(f"Added On: {added_on}\n")
+            print(f'DELETING {torrent.name} ({c}) ({torrent.state}) ({torrent.tags})')
+            qbt_client.torrents_delete(delete_files=DELETE_FILES, torrent_hashes=torrent.hash)
+
+            
+
+    print(f"Deleted {count} Torrents")
 
 if __name__ == "__main__":
     main()
